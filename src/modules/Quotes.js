04e9a2370c2d81283f396291eb442ecc7d6cc806
PiotrSiryk <Piotr.Siryk@gmail.com>
2021-02-08 01:23:10 +0100

    quote json and fixes

--- src/modules/Quotes.js
+++ src/modules/Quotes.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ben from "../images/photos/ben.jpg";
+import { quotes } from "../quotes.json";
 
 export default function Quotes() {
+  console.log(quotes);
   const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   const styles = {
     position: "absolute",
@@ -10,10 +12,10 @@ export default function Quotes() {
   return (
     <main className="quotes-main">
       <div className="quotes">
-        {arr.map((element, index) => {
+        {quotes.map((element, index) => {
           const topAl = `${Math.random() * 150}px`;
-          const leftAl = `${Math.random() * 150}px`;
-          const transform = `rotate(${Math.random() * 160 - 80}deg)`;
+          const leftAl = `${Math.random() * 110}px`;
+          const transform = `rotate(${Math.random() * 140 - 70}deg)`;
           return (
             <section key={index}>
               <div
@@ -26,20 +28,17 @@ export default function Quotes() {
                   transition: "1200ms",
                 }}
                 onMouseEnter={(event) => {
-                  event.target.style = `left:${leftAl};top:${topAl};position: absolute;transform: scale(1.8); z-index: 999`;
+                  event.target.style = `left:${leftAl};top:${topAl};position: absolute;transform: scale(1.6); z-index: 999`;
                 }}
                 onMouseLeave={(event) => {
                   event.target.style = `left:${leftAl};top:${topAl};position: absolute;transform: ${transform}`;
                 }}
               >
-                <img src={ben} alt="" className="photo" />
+                <img src={element.image} alt="" className="photo" />
                 <div>
                   <div>
-                    <p>
-                      "Beer is proof that God loves us and wants us to be
-                      happy."
-                    </p>
-                    <h5>-Benjamin Franklin</h5>
+                    <p>{element.body}</p>
+                    <h5>-{element.name}</h5>
                   </div>
                 </div>
               </div>
