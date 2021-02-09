6eb4ba05e518ea386cd7a304c0d0d8e3b8979fe2
PiotrSiryk <Piotr.Siryk@gmail.com>
2021-02-09 23:54:17 +0100

    base home

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import NavBar from "./modules/NavBar";
 import Main from "./modules/Main";
 import Quotes from "./modules/Quotes";
+import Home from "./modules/Home";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 function App() {
@@ -12,6 +13,9 @@ function App() {
         </Route>
         <Route exact path="/drinks">
           <Main />
+        </Route>
+        <Route exact path="/">
+          <Home />
         </Route>
         <Route path="/quotes">
           <Quotes />
---
+++ src/modules/Home.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="home">
+      <header></header>
+      <main>
+        <div className="thing-to-do">
+          <h2>Karaoke Night</h2>
+          <img
+            src="https://media-cdn.tripadvisor.com/media/photo-p/13/22/52/bb/steak.jpg"
+            alt=""
+          />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+            excepturi, perferendis sequi mollitia repudiandae veritatis.
+            Voluptatem nulla officiis minus, quae, quis ducimus voluptatum ullam
+            eligendi sint animi perferendis, iusto possimus.
+          </p>
+        </div>
+      </main>
+    </div>
+  );
+}
